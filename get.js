a7ed8d8bef6fda968701a022e1cce77c914ebb8c
Michael Klein <miketheklein@gmail.com>
2018-07-17 13:52:25 -0700

    getting get to work

--- get.js
+++ get.js
@@ -3,21 +3,32 @@ import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context, callback) {
   const params = {
-    TableName: "notes",
+    TableName: "note",
+    // 'Key' defines the partition key and sort key of the item to be retrieved
     Key: {
-      userId: event.requestContext.identity.congnitoIdentityId,
+      // - 'userId': Identity Pool identity id of the authenticated user
+      userId: event.requestContext.identity.cognitoIdentityId,
+      // - 'noteId': path parameter
       noteId: event.pathParameters.id
     }
-  }
+  };
 
   try {
     const result = await dynamoDbLib.call("get", params);
     if (result.Item) {
+      // Return the retrieved item
       callback(null, success(result.Item));
     } else {
-      callback(null, failure({ status: false, error: "Item not found." }));
+      callback(
+        null,
+        failure({
+          status: false,
+          error: "Item not found."
+        })
+      );
     }
   } catch (e) {
+    console.log("[ERROR]", e);
     callback(null, failure({ status: false }));
   }
 }
